bd6a97bd9205e27973e1e5e3a4fa5d60a05cb23a
Aniket Singh <singhdharmvir81@gmail.com>
2022-01-12 14:29:47 +0530

    added new header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   ],
   theme: {
     extend: {},
+      backgroundImage: {
+        // 'header-hero': "url(https:source.unsplash.com/random/1920x1080)",
+        wave:"url(/assets/wave.svg)",
+      },
   },
   plugins: [],
 }
