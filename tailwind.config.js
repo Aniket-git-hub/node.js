8b54703ece6dc5dcf02c3a7fc1bb21a7abac1e33
Aniket Singh <singhdharmvir81@gmail.com>
2022-01-14 10:24:48 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         // 'header-hero': "url(https:source.unsplash.com/random/1920x1080)",
         wave:"url(/assets/wave.svg)",
         random:"url(/assetshttps:source.unsplash.com/random/400400)",
+        svg:"url(https://source.unsplash.com/random/400x400)",
       },
   },
   plugins: [],
